d18634585e5163f9b428f4233da2278ded5f6839
Denys Vasyliuk <denisvasiluk@gmail.com>
2026-07-29 21:39:42 +0200

    Initialize Three.js scene with a rotating sphere

--- ira-performance/main.js
+++ ira-performance/main.js
@@ -1,14 +1,92 @@
-document.getElementById("container").innerHTML = `
-<div style="
-height:100vh;
-display:flex;
-align-items:center;
-justify-content:center;
-background:black;
-color:white;
-font-size:40px;
-font-family:sans-serif;
-">
-JS WITHOUT MODULE WORKS
-</div>
-`;
+const container =
+document.getElementById("container");
+
+
+const scene =
+new THREE.Scene();
+
+
+scene.background =
+new THREE.Color(0x050505);
+
+
+
+const camera =
+new THREE.PerspectiveCamera(
+45,
+window.innerWidth / window.innerHeight,
+0.1,
+100
+);
+
+
+camera.position.z = 5;
+
+
+
+const renderer =
+new THREE.WebGLRenderer({
+    antialias:false
+});
+
+
+renderer.setSize(
+window.innerWidth,
+window.innerHeight
+);
+
+
+container.appendChild(
+renderer.domElement
+);
+
+
+
+const geometry =
+new THREE.SphereGeometry(
+1.5,
+32,
+32
+);
+
+
+
+const material =
+new THREE.MeshBasicMaterial({
+    color:0xffffff
+});
+
+
+
+const sphere =
+new THREE.Mesh(
+geometry,
+material
+);
+
+
+scene.add(
+sphere
+);
+
+
+
+function animate(){
+
+requestAnimationFrame(
+animate
+);
+
+
+sphere.rotation.y +=0.01;
+
+
+renderer.render(
+scene,
+camera
+);
+
+}
+
+
+animate();
